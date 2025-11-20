11841b1715829290f69c6dc4ca371fb0c5a4639a
elya <Elnaradyuzheva@gmail.com>
2025-11-20 14:16:54 +0200

    feature/ISSUE-4: change the ports from https to http

--- Dockerfile
+++ Dockerfile
@@ -3,14 +3,7 @@ FROM eclipse-temurin:21-jdk
 WORKDIR /app
 
 COPY build/libs/*.jar app.jar
-COPY keystore.p12 ./keystore.p12
 
-ENV SSL_KEY_STORE_PASSWORD=${SSL_KEY_STORE_PASSWORD}
-ENV SSL_KEY_STORE=./keystore.p12
-ENV SSL_KEY_ALIAS=${SSL_KEY_ALIAS}
-ENV NGROK_AUTH_TOKEN=${NGROK_AUTHTOKEN}
-
-
-EXPOSE 8443
+EXPOSE 8080
 
 ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/com/bytebook/bfftg/controller/WebHookController.java
+++ src/main/java/com/bytebook/bfftg/controller/WebHookController.java
@@ -1,20 +1,19 @@
 package com.bytebook.bfftg.controller;
 
+
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 public class WebHookController {
 
     @PostMapping("/bot")
     public ResponseEntity<String> onUpdateReceived(@RequestBody String update) {
-        System.out.println("Получен update: " + update);
+        log.info("Получен update: {}", update);
         return ResponseEntity.ok("OK");
     }
 
-    @GetMapping("/bot")
-    public String test() {
-        return "Webhook endpoint is alive!";
-    }
 
 }
